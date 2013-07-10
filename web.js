b0bd960be18a915d76ee0b408c0a603e8d923a47
Gu Xiangguo <guxiangguo@gmail.com>
2013-07-10 11:21:04 +0800

    reading index.html in web.js

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var infile = "index.html"
+  var contents = fs.readFileSync(infile); 
+  response.send(contents.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
